feb74a5f97fc3e228e5032a9ee5a5d0a37a23c5d
Emiliano <cajademensajes@outlook.es>
2022-06-11 16:51:44 -0500

    making http requests

--- JavascriptRecap/fetchAPI.js
+++ JavascriptRecap/fetchAPI.js
@@ -1,5 +1,33 @@
 const url = 'https://jsonplaceholder.typicode.com/users';
 
-fetch(url).then((response) => {
-    console.log(response);
-})
+//This is the old way to use the requests, it still used.
+//this is functional code
+// fetch(url,{
+//     method: 'POST',//POST, GET, UPDATE/PATCH, DELETE
+//     headers:{
+//         'Content-type':'application/json',
+//         'Authorization':'Bearer acadebieeseexistiruntokendeautorizacion'
+//     },
+//     body: JSON.stringify({
+//         name: 'Chanchito Feliz',
+//         website: 'www.google.com'
+//     })
+// }).then((response) => {
+//     return response.json()
+// }).then(data => console.log(data));
+
+//This is the new way to call requests
+//This is imperative code
+const makeRequest = async () => {
+    const response = await fetch(url,{
+        method: 'POST',
+        headers:{
+            'Content-type':'application/json',
+            'Authorization': 'Bearer acadebieeseexistiruntokendeautorizacion'
+        },
+        body: JSON.stringify({name: 'Chanchito alegre', website: 'www.google.com'})
+    });
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+}
+makeRequest();
